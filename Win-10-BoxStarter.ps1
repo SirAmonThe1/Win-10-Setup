ddcb9dedd0433de0824793bb1a1564e9ff873116
SirAmonThe1 <64291960+SirAmonThe1@users.noreply.github.com>
2020-05-20 15:00:44 +0200

    test

--- Win-10-BoxStarter.ps1
+++ Win-10-BoxStarter.ps1
@@ -2,6 +2,40 @@
 # PREREQUISITES
 #####################
 
+# Settings
+$repoUri = 'https://github.com/PapaDio/setup.git'
+$setupPath = "./Dio-setup"
+
+Push-Location "/"
+
+# Adjust the execution policy for a programming environment
+Set-ExecutionPolicy Unrestricted -Scope LocalMachine -Force
+
+# Install chocolately, the minimum requirement
+Set-ExecutionPolicy Bypass -Scope Process -Force
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Clean if necessary
+if (Test-Path -Path $setupPath) {
+    Remove-Item $setupPath -Recurse -Force
+}
+
+# Install git
+& choco install git --confirm --limit-output
+
+# Reset the path environment
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+
+# Clone the setup repository
+& git clone $repoUri $setupPath
+
+# Enter inside the repository and invoke the real set-up process
+
+
+
+
+
+
 Set-ExplorerOptions -showHiddenFilesFoldersDrives -showProtectedOSFiles -showFileExtensions
 Set-TaskbarSmall
 
